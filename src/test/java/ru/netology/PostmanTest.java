e1932176d88c8a10bd31b8a5fe55edb027a1b6c6
venomek <venom4ek@gmail.com>
2020-06-07 09:20:53 +0300

    initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void shouldPostMethod() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("date", equalTo("some data"))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+}
